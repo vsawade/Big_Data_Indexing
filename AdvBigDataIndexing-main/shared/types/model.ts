5d9c1a678d24beac43ff09b22d62c9ec58b05255
vsawade <vinaysawade1@gmail.com>
2025-07-12 10:52:30 -0700

    Delete AdvBigDataIndexing-main directory

--- AdvBigDataIndexing-main/shared/types/model.ts
+++
@@ -1,9 +0,0 @@
-import { SchemaModel } from './schema.model'
-
-export type Model = {
-  fallbackSchema?: SchemaModel
-  schema: () => Promise<string | null>
-  fallbackElasticMappings?: object
-  elasticMappings: () => Promise<string | null>
-  key: string
-}
